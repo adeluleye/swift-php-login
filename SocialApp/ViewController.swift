126509b19b21f40bd2fea77217c96ef0a850f326
adeluleye <adelu4real@gmail.com>
2018-10-02 22:36:33 +0100

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/2/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
